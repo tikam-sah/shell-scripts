39e3210ac46eb5c4b97f07bdbf7b4cd035178a18
Tikam Sah <101312122+tikam-sah@users.noreply.github.com>
2024-09-15 19:17:53 +0530

    Update anydesk.sh

--- anydesk.sh
+++ anydesk.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Define the URL for the AnyDesk Debian package
-ANYDESK_URL="https://download.anydesk.com/linux/anydesk_6.3.1-1_amd64.deb"
+# Define the URL for the latest AnyDesk Debian package
+ANYDESK_URL="https://download.anydesk.com/linux/anydesk_amd64.deb"
 
 # Define the filename for the downloaded package
-DEB_FILE="anydesk.deb"
+DEB_FILE="anydesk_latest.deb"
 
 # Update package list
 echo "Updating package list..."
@@ -14,13 +14,25 @@ sudo apt update
 echo "Downloading AnyDesk..."
 wget -O $DEB_FILE $ANYDESK_URL
 
+# Check if wget succeeded
+if [ $? -ne 0 ]; then
+    echo "Failed to download AnyDesk package. Exiting."
+    exit 1
+fi
+
 # Install AnyDesk
 echo "Installing AnyDesk..."
 sudo dpkg -i $DEB_FILE
 
-# Fix any missing dependencies
-echo "Fixing dependencies..."
-sudo apt-get install -f
+# Check if dpkg succeeded
+if [ $? -ne 0 ]; then
+    echo "Failed to install AnyDesk. Fixing dependencies..."
+    sudo apt-get install -f
+    if [ $? -ne 0 ]; then
+        echo "Failed to fix dependencies. Exiting."
+        exit 1
+    fi
+fi
 
 # Clean up
 echo "Cleaning up..."
